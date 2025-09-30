b152171ddf103add9ca621684c5e4c5d7d3ab6c4
David <dexplop2602@ieszaidinvergeles.org>
2025-09-30 13:12:44 +0200

    Borro los ejercicios que no eran necesarios

--- AreayPerimetro.py
+++
@@ -1,7 +0,0 @@
-base = float(input("Base: "))
-altura = float((input("Altura: ")))
-
-Area = base * altura 
-Perimetro = base * 2 + altura * 2
-print(f"El area es: {Area} El perimetro es: {Perimetro}")
-
--- Prueba.py
+++
@@ -1,4 +0,0 @@
-peso = float(input("Peso (kg): "))
-altura = float(input("Altura (m): "))
-resultado = peso / altura ** 2
-print("El resultado es:", resultado)
