c705260b1da5f69aab04d4155cdb0520173d9ed0
David <dexplop2602@ieszaidinvergeles.org>
2025-10-07 13:31:37 +0200

    Ejercicio 2 Tanda 2
    Escribir un nombre y mostrarlo de diferentes formas

---
+++ Segunda Tanda/ejercicio2.py
@@ -0,0 +1,14 @@
+""" Ejercicio 2
+Escribir un programa que pregunte el nombre completo del usuario en la consola y
+después muestre por pantalla el nombre completo del usuario tres veces, una con
+todas las letras minúsculas, otra con todas las letras mayúsculas y otra solo con la
+primera letra del nombre y de los apellidos en mayúscula. El usuario puede
+introducir su nombre combinando mayúsculas y minúsculas como quiera."""
+
+nombre = str(input("Introduzca su nombre completo: "))
+
+resultado = nombre.lower()
+resultadomayus = nombre.upper() 
+resultadocap = nombre.title() 
+
+print (f"{resultado}, {resultadomayus}, {resultadocap}")
