90ebee318826843f002d28e2ea9832e164af1f49
David <dexplop2602@ieszaidinvergeles.org>
2025-10-14 13:16:11 +0200

    Carpeta Tanda 3 y Ejercicio 1 Tanda 3
    Aqui he subido la carpeta de la tande y el ejercicio 1 para mostrar que persona es menor o mayor de edad

---
+++ Tercera Tanda/ejercicio1.py
@@ -0,0 +1,10 @@
+"""Ejercicio 1
+Escribir un programa que pregunte al usuario su edad y muestre por pantalla si es
+mayor de edad o no"""
+
+edad = float(input("Escriba su edad: "))
+
+if edad >= 18:
+    print("Usted es mayor de edad")
+else:
+    print("Usted es menor de edad")
