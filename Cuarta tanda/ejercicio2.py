4414adc3afaeb3d65a67e4c2875f030e24e00db6
David <dexplop2602@ieszaidinvergeles.org>
2025-10-21 13:54:35 +0200

    Ejercicio 2 Tanda 4
    Mostrar la edad del 1 al numero de la edad

---
+++ Cuarta tanda/ejercicio2.py
@@ -0,0 +1,11 @@
+"""Ejercicio 2
+Escribir un programa que pregunte al usuario su edad y muestre por pantalla todos
+los años que ha cumplido (desde 1 hasta su edad)"""
+
+edad = float(input("Escriba su edad: "))
+
+b = 1
+a = 1
+while a <= edad:
+    print(a)
+    a += 1
