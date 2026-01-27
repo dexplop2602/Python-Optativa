19622098b2007eb762a3f823c8de220437474377
David <dexplop2602@ieszaidinvergeles.org>
2026-01-27 13:12:10 +0100

    Ejercicio 2 Tanda 7

---
+++ Tanda 07/ejercicio2.py
@@ -0,0 +1,8 @@
+"""Ejercicio 2
+Escribir una función a la que se le pase una cadena <nombre> y muestre por
+pantalla el saludo ¡hola <nombre>!."""
+
+def saludo(nombre):
+    return print(f"!Hola {nombre}!")
+
+saludo("Manolin")
